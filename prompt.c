30e1df67e05a53a349b068b31331dca9a36a4ac4
JOSH MERRICK <moshjerrick@gmail.com>
2024-03-26 09:34:18 -0500

    prompt 2

--- prompt.c
+++ prompt.c
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv)
+int main2(int argc, char **argv)
 {
 	if (isatty(STDIN_FILENO))
 	{
 		char cwd[1024];
-		getcwd(cwd, sizeof(cwd)):
-		char *username = getloging();
+		getcwd(cwd, sizeof(cwd));
+		char *username = "test\0";
 		char hostname[1024];
 		gethostname(hostname, sizeof(hostname));
 		printf("%s@%s:%s$", username, hostname, cwd);
---
+++ prompt2.c
@@ -0,0 +1,71 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+void shloop(void);
+char *read_line(void);
+void parse_line(char*);
+
+int main(int argc, char **argv)
+{
+    shloop ();
+    return (0);
+}
+
+void shloop (void)
+{
+	char *line;
+	char **args;
+	int status = 1;
+
+	while (status)
+	{
+		printf("# ");
+        line = read_line();
+		if (line == NULL)
+		{
+			status = 0;
+			continue;
+		}
+		pid_t pid = fork();
+		if (pid <0)
+		{
+			perror("fork failed");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+
+			{
+				parse_line(line);
+				exit(EXIT_SUCCESS);
+
+			}
+	else
+	{
+		wait(NULL);
+	}
+		free(line);
+	}
+
+}
+
+char *read_line(void)
+{
+    char *line = NULL;
+    ssize_t bufsize = 0;
+    if (getline(&line, &bufsize, stdin) == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+
+
+    return(line);
+}
+
+void parse_line(char *line)
+{
+	printf("%s",  line);
+}
