7fe63cd69cfd1948fa2d0f70257fd8833b66e103
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-01 15:40:42 -0500

    changed exit message

--- prompt.c
+++ prompt.c
@@ -69,7 +69,7 @@ void parse_line(char *line)
     {
         if (strcmp(args[0], "exit") == 0)
         {
-            printf("later skater\n");
+            printf("OK\n");
             exit(EXIT_SUCCESS);
         }
         else if (strcmp(args[0], "cd") == 0)
